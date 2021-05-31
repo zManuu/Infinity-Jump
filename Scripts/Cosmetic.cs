dfd815f705c56577f3e8e542bb5a854ce02efb26
zManuu <60520810+zManuu@users.noreply.github.com>
2021-05-31 16:28:48 +0200

    Cosmetic script fixxes; new cosmetic type; new cosmetics
    New cosmetic type: Hoodie / Pullover
    New cosmetics: gucci hoodie, camo green- & blue hoodie

--- Scripts/Cosmetic.cs
+++ Scripts/Cosmetic.cs
@@ -7,6 +7,7 @@ public class Cosmetic : ScriptableObject
 
     public CosmeticType type;
     public Sprite sprite;
+    public Sprite preview;
     public Vector2 preferedSizeImage;
     public int price;
 
--- Scripts/CosmeticController.cs
+++ Scripts/CosmeticController.cs
@@ -9,31 +9,33 @@ public class CosmeticController : MonoBehaviour
     private Cosmetic[] cosmetics;
 
     [SerializeField] private bool isCosmeticSelection;
-    [SerializeField] private Image imageHat, imageCape, imageShoes;
-    [SerializeField] private Text priceHat, priceCape, priceShoes;
-    [SerializeField] private Text buyHat, buyCape, buyShoes;
+    [SerializeField] private Image imageHat, imageChest, imageShoes;
+    [SerializeField] private Text priceHat, priceChest, priceShoes;
+    [SerializeField] private Text buyHat, buyChest, buyShoes;
     [SerializeField] private Text coinIndicator;
 
     private List<Cosmetic> unlockedCosmetics;
     private Cosmetic activeHat;
-    private Cosmetic activeCape;
+    private Cosmetic activeChest;
     private Cosmetic activeShoes;
     private int currentHat = 0;
-    private int currentCape;
+    private int currentChest = 3;
     private int currentShoes;
     private SpriteRenderer rendererHat;
-    private SpriteRenderer rendererCape;
+    private SpriteRenderer rendererChest;
     private SpriteRenderer rendererShoes;
 
     private void Start()
     {
+        PlayerPrefs.SetInt("Coins", 10000);
         if (!PlayerPrefs.HasKey("Cosmetic_Unlocked_0"))
         {
             PlayerPrefs.SetString("Cosmetic_Unlocked_0", true.ToString());
+            PlayerPrefs.SetString("Cosmetic_Unlocked_3", true.ToString());
         }
 
         unlockedCosmetics = new List<Cosmetic>();
-        for (int i=0; i<30; i++)
+        for (int i=0; i<cosmetics.Length; i++)
         {
             if (bool.Parse(PlayerPrefs.GetString("Cosmetic_Unlocked_" + i, "false")))
             {
@@ -42,15 +44,16 @@ public class CosmeticController : MonoBehaviour
         }
 
         activeHat = cosmetics[PlayerPrefs.GetInt("Cosmetic_Active_Hat")];
-        //activeCape = cosmetics[PlayerPrefs.GetInt("Cosmetic_Active_Cape")];
+        Debug.Log(PlayerPrefs.GetInt("Cosmetic_Active_Chest"));
+        activeChest = cosmetics[PlayerPrefs.GetInt("Cosmetic_Active_Chest")];
         //activeShoes = cosmetics[PlayerPrefs.GetInt("Cosmetic_Active_Shoes")];
 
         if (isCosmeticSelection)
         {
             currentHat = GetIndex(activeHat);
             UpdateView(CosmeticType.Hat);
-            //currentCape = GetIndex(activeCape);
-            //UpdateView(CosmeticType.Cape);
+            currentChest = GetIndex(activeChest);
+            UpdateView(CosmeticType.Chest);
             //currentShoes = GetIndex(activeShoes);
             //UpdateView(CosmeticType.Shoes);
         }
@@ -58,8 +61,9 @@ public class CosmeticController : MonoBehaviour
         {
             rendererHat = GameObject.Find("Player").transform.GetChild(2).GetComponent<SpriteRenderer>();
             rendererHat.sprite = activeHat.sprite;
-            //rendererCape = GameObject.Find("Player").transform.GetChild(3).GetComponent<SpriteRenderer>();
-            //rendererShoes = GameObject.Find("Player").transform.GetChild(4).GetComponent<SpriteRenderer>();
+            rendererChest = GameObject.Find("Player").transform.GetChild(3).GetComponent<SpriteRenderer>();
+            rendererChest.sprite = activeChest.sprite;
+            rendererShoes = GameObject.Find("Player").transform.GetChild(4).GetComponent<SpriteRenderer>();
         }
     }
 
@@ -70,7 +74,7 @@ public class CosmeticController : MonoBehaviour
             PlayerPrefs.SetString("Cosmetic_Unlocked_" + GetIndex(unlockedCosmetic), "true");
         });
         PlayerPrefs.SetInt("Cosmetic_Active_Hat", GetIndex(activeHat));
-        PlayerPrefs.SetInt("Cosmetic_Active_Cape", GetIndex(activeCape));
+        PlayerPrefs.SetInt("Cosmetic_Active_Chest", GetIndex(activeChest));
         PlayerPrefs.SetInt("Cosmetic_Active_Shoes", GetIndex(activeShoes));
     }
 
@@ -99,6 +103,15 @@ public class CosmeticController : MonoBehaviour
         }
         return c;
     }
+    private int GetFirstIndexOf(CosmeticType type)
+    {
+        for (int i=0; i<cosmetics.Length; i++)
+        {
+            if (cosmetics[i].type == type)
+                return i;
+        }
+        return -1;
+    }
     private void UpdateView(CosmeticType type)
     {
         switch (type)
@@ -117,6 +130,21 @@ public class CosmeticController : MonoBehaviour
                     buyHat.text = "Buy";
                 }
                 break;
+            case CosmeticType.Chest:
+                Cosmetic newChest = cosmetics[currentChest];
+                imageChest.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, newChest.preferedSizeImage.x);
+                imageChest.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, newChest.preferedSizeImage.y);
+                imageChest.sprite = newChest.preview;
+                priceChest.text = string.Format("{0} [{1}]", newChest.name, newChest.price.ToString());
+                if (unlockedCosmetics.Contains(newChest))
+                {
+                    buyChest.text = "Select";
+                }
+                else
+                {
+                    buyChest.text = "Buy";
+                }
+                break;
         }
     }
 
@@ -133,7 +161,7 @@ public class CosmeticController : MonoBehaviour
     }
     public void OnLastHat()
     {
-        if (currentHat == 0)
+        if (currentHat == GetFirstIndexOf(CosmeticType.Hat))
         {
             return;
         }
@@ -160,10 +188,49 @@ public class CosmeticController : MonoBehaviour
         Save();
     }
 
+    public void OnNextChest()
+    {
+        if (currentChest == (GetCosmeticCount(CosmeticType.Chest) + GetCosmeticCount(CosmeticType.Hat)) - 1)
+        {
+            return;
+        }
+        currentChest++;
+        UpdateView(CosmeticType.Chest);
+    }
+    public void OnLastChest()
+    {
+        if (currentChest == GetFirstIndexOf(CosmeticType.Chest))
+        {
+            return;
+        }
+        currentChest--;
+        UpdateView(CosmeticType.Chest);
+    }
+    public void OnSelectChest()
+    {
+        Cosmetic c = cosmetics[currentChest];
+        if (unlockedCosmetics.Contains(c))
+        {
+            activeChest = c;
+        }
+        else
+        {
+            if (CoinManagement.GetCoins() >= c.price)
+            {
+                CoinManagement.RemoveCoins(c.price);
+                unlockedCosmetics.Add(c);
+                activeChest = c;
+                UpdateView(CosmeticType.Chest);
+                coinIndicator.text = CoinManagement.GetCoins().ToString();
+            }
+        }
+        Save();
+    }
+
     public enum CosmeticType
     {
         Hat,
-        Cape,
+        Chest,
         Shoes
     }
 
